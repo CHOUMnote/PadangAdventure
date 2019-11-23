812de12f5b617b48b786b1848cce6135dc21517d
KR-CHOUM <rlawlgh1028@naver.com>
2019-11-23 17:44:32 +0900

    100%!

--- app/src/main/java/com/example/padangadventure/MainGame.java
+++ app/src/main/java/com/example/padangadventure/MainGame.java
@@ -122,9 +122,9 @@ public class MainGame extends AppCompatActivity {
         }
     }
 
-    //@Override
-    //public void onBackPressed() {
-    //    //안드로이드 백버튼 막기
-      //  return;
-    //}
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }
 }
